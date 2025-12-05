e907a56518219c2d42218c9902293c0ebeb6b4f8
Dokusen <ChrisTsikrikes@gmail.com>
2025-12-05 18:34:06 +0200

    Remove duplicate login event handling code

--- LuaCourses/FourthCourse.lua
+++ LuaCourses/FourthCourse.lua
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 local login = CreateFrame ("Frame")
 login:RegisterEvent("PLAYER_LOGIN")
 login:SetScript("OnEvent", function (self, event, ...)
     local characterName = UnitName("player")
     print(characterName)
-=======
-local login = CreateFrame("Frame")
-login:RegisterEvent("PLAYER_LOGIN")
-login:SetScript("OnEvent", function(self, event, ...)
-      local characterName = UnitName("player")
-      print(characterName)
->>>>>>> eba3e5743dc48c1b5c368a41b069c9ce2ad0d0ef
 end)
