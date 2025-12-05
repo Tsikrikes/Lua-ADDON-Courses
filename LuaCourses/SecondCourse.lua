d929740de2908f180cdd2c9ae0dfb3d1714fe829
Dokusen <ChrisTsikrikes@gmail.com>
2025-12-05 18:32:25 +0200

    Changes

--- LuaCourses/SecondCourse.lua
+++ LuaCourses/SecondCourse.lua
@@ -1,6 +1,6 @@
 -- An addon that greets the user with a message when they type /hello
 
-SLASH_HELLO1 = "/hello"
+SLASH_HELLO1 = "/hi"
 SlashCmdList["HELLO"] = function(msg)
     if msg == "" then
         print("Hello world", msg)
